53387a3dc7fe663c9b7792c137af5d75764e6aba
hayate0721 <h08066802495@gmail.com>
2023-07-30 10:52:28 +0900

    Analise datas by using dataSet operations

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,34 @@ public class Main {
         Set<Task> annstTasks = TaskData.getTasks("Ann");
         sortAndPrint("Ann's Tasks", tasks);
 
+        Set<Task> bobsTasks = TaskData.getTasks("Bob");
+        Set<Task> carolsTasks = TaskData.getTasks("Carol");
+        List<Set<Task>> sets = List.of(annstTasks, bobsTasks, carolsTasks);
+
+        Set<Task> assignedTasks = getUnionList(sets);
+        sortAndPrint("Assigned Tasks", assignedTasks);
+
+        Set<Task> everyTask = getUnionList(List.of(tasks,assignedTasks));
+        sortAndPrint("The True All Tasks", everyTask);
+
+        Set<Task> missingTasks = getDifference(everyTask, tasks);
+        sortAndPrint("Missing Tasks", missingTasks);
+
+        Set<Task> unassignedTasks = getDifference(tasks, assignedTasks);
+        sortAndPrint("Unassigned Tasks", unassignedTasks);
+
+        Set<Task> overlap = getUnionList(List.of(
+                getIntersect(annstTasks, bobsTasks),
+                getIntersect(bobsTasks, carolsTasks),
+                getIntersect(annstTasks, carolsTasks)));
+        sortAndPrint("Assigned to Multiples", overlap);
+
+        List<Task> overlapping = new ArrayList<>();
+        for(Set<Task> set : sets){
+            Set<Task> dupes = getIntersect(set, overlap);
+            overlapping.addAll(dupes);
+        }
+        sortAndPrint("Overlapping", overlapping);
     }
 
     private static void sortAndPrint(String header, Collection<Task> collection){
@@ -24,5 +52,25 @@ public class Main {
         List<Task> list = new ArrayList<>(collection);
         list.sort(sorter);
         list.forEach(System.out::println);
+    }
+
+    private static Set<Task> getUnionList(List<Set<Task>> sets){
+        Set<Task> union = new HashSet<>();
+        for(var taskSet : sets){
+            union.addAll(taskSet);
+        }
+        return union;
+    }
+
+    private static Set<Task> getIntersect(Set<Task> a, Set<Task> b){
+        Set<Task> intersect = new HashSet<>(a);
+        intersect.retainAll(b);
+        return intersect;
+    }
+
+    private static Set<Task> getDifference(Set<Task> a, Set<Task> b){
+        Set<Task> result = new HashSet<>(a);
+        result.removeAll(b);
+        return  result;
     }
 }
